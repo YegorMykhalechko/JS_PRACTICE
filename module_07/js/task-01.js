c6a8591a440b717485a38c57ccaff0d2fedd246f
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2021-04-03 21:49:03 +0200

    DOM

---
+++ module_07/js/task-01.js
@@ -0,0 +1,77 @@
+'use strict'
+function createBlockFirst() {
+    const item = document.createElement('li');
+    item.classList.add('item')
+    const header = document.createElement('h2');
+    header.textContent = 'Животные';
+    const list = document.createElement('ul');
+    const listItemFirst = document.createElement('li');
+    listItemFirst.textContent = 'Кот';
+    const listItemSecond = document.createElement('li');
+    listItemSecond.textContent = 'Хомяк';
+    const listItemThird = document.createElement('li');
+    listItemThird.textContent = 'Лошадь';
+    const listItemFourth = document.createElement('li');
+    listItemFourth.textContent = 'Попугай';
+    list.append(listItemFirst, listItemSecond, listItemThird, listItemFourth);
+    item.append(header, list);
+    return item;
+}
+const itemFirst = createBlockFirst();
+function createBlockSecond() {
+    const item = document.createElement('li');
+    item.classList.add('item')
+    const header = document.createElement('h2');
+    header.textContent = 'Продукты';
+    const list = document.createElement('ul');
+    const listItemFirst = document.createElement('li');
+    listItemFirst.textContent = 'Хлеб';
+    const listItemSecond = document.createElement('li');
+    listItemSecond.textContent = 'Петрушка';
+    const listItemThird = document.createElement('li');
+    listItemThird.textContent = 'Творог';
+    list.append(listItemFirst, listItemSecond, listItemThird);
+    item.append(header, list);
+    return item;
+}
+const itemSecond = createBlockSecond();
+function createBlockThird() {
+    const item = document.createElement('li');
+    item.classList.add('item')
+    const header = document.createElement('h2');
+    header.textContent = 'Технологии';
+    const list = document.createElement('ul');
+    const listItemFirst = document.createElement('li');
+    listItemFirst.textContent = 'HTML';
+    const listItemSecond = document.createElement('li');
+    listItemSecond.textContent = 'CSS';
+    const listItemThird = document.createElement('li');
+    listItemThird.textContent = 'JavaScript';
+    const listItemFourth = document.createElement('li');
+    listItemFourth.textContent = 'React';
+    const listItemFirth = document.createElement('li');
+    listItemFirth.textContent = 'Node';
+    list.append(listItemFirst, listItemSecond, listItemThird, listItemFourth, listItemFirth);
+    item.append(header, list);
+    return item;
+}
+const itemThird = createBlockThird();
+const categories = document.querySelector('ul');
+categories.append(itemFirst, itemSecond, itemThird);
+
+// const task = document.querySelectorAll('li.item');
+// console.log(`В списке ${task.length} категории.`);
+
+// const task = document.querySelectorAll('h2');
+// task.forEach(item => {
+//     console.log(item.firstChild);
+//     console.log(item.nextSibling.children.length);
+// })
+
+
+
+
+
+
+
+
